d2ed89a5901e16f24d6820719210d0fbceb9081b
nick <nicholas.a.downey@gmail.com>
2022-12-01 02:35:57 -0500

    1-7 Complete, Needs Refactor

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...theArgs) {
+  let theArray = arguments[0];
+  console.log(`ARRAY: ${theArray}`);
+  for (arg of theArgs) {
+    console.log(`ARG: ${arg}`);
+    if (arg === theArgs[0]) {
+    } else {
+      if ((indexValue = theArray.indexOf(arg)) != -1) {
+        console.log(`INDEX: ${indexValue}`);
+        theArray.splice(indexValue, 1);
+      }
+    }
+  }
+  return theArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1, 2, 3, 4], 3));
+// console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
