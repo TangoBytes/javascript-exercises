e60aeff50e3efb757ebbe5d06583652e363cf8c9
nicholasdowney <88251016+nicholasdowney@users.noreply.github.com>
2022-12-12 01:45:03 -0500

    Left off at palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce(function (total, num) {
+    total += num;
+    return total;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce(function (total, num) {
+    total *= num;
+    return total;
+  });
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) {
+    return -1;
+  }
+  if (num < 2) {
+    return 1;
+  }
+  let nums = [];
+  for (i = num - 1; i > 0; i--) {
+    nums.push(i);
+  }
+  let total = nums.reduce(function (total, currentNum) {
+    return total * currentNum;
+  }, num);
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+  palindrome = palindrome.toLowerCase();
+  palindrome = palindrome.replace(/[^a-z0-9]/gi, "");
+  palindrome = palindrome.split("");
+  forwardPalindrome = [...palindrome];
+  forwardPalindrome = forwardPalindrome.join("");
+  palindrome.reverse();
+  palindrome = palindrome.join("");
+  console.log(forwardPalindrome);
+  console.log(palindrome);
 
+  return forwardPalindrome == palindrome;
 };
 
 // Do not edit below this line
