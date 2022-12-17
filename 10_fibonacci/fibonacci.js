86876efcb953b831de183f67e04796241a9956fd
nicholasdowney <88251016+nicholasdowney@users.noreply.github.com>
2022-12-17 14:00:36 -0500

    Solved until 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+  if (typeof num != Number) {
+    num = Number(num);
+  }
+  let fibonacciArray = [1];
+  while (!fibonacciArray[num - 1]) {
+    for (let i = 0; i < 500; i++) {
+      if (!fibonacciArray[i - 1]) {
+        firstNum = 0;
+      } else {
+        firstNum = fibonacciArray[i - 1];
+      }
+      fibonacciArray.push(firstNum + fibonacciArray[i]);
+    }
+  }
+  return fibonacciArray[num - 1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
-};
+function findTheOldest(people) {
+  oldestPerson = people.reduce((oldest, person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+    console.log("START: PERSON IS NOW", person);
+    console.log("START: OLDEST IS NOW", oldest);
+    person.age = person.yearOfDeath - person.yearOfBirth;
+    if (!oldest.age) {
+      oldest = person;
+    }
+    if (person.age > oldest.age) {
+      oldest = person;
+    }
+    console.log("END: PERSON IS NOW", person);
+    console.log("END: OLDEST IS NOW", oldest);
+    return oldest;
+  }, {});
+  return oldestPerson;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
